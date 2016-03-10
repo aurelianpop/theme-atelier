5c63bd53c5f28f56ca37a49fbbe28be223f18b44
Ingrid <kulcsar_19@yahoo.com>
2016-03-10 23:24:43 +0200

    Subfooter

--- footer.php
+++ footer.php
@@ -11,25 +11,32 @@
 
 ?>
 
-	</div><!-- #content -->
+</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="footer-container row grey darken-3">
-			<?php dynamic_sidebar( 'at_footer_area' ); ?>
-		</div>
-		<div class="footer-container-small">
-			<div class="social-container grey darken-3">
-				<ul>
-					<?php
-					$social_links = get_option('atelier_social_media_options');
-					echo !empty($social_links['facebook_link']) ? '<li class="waves-effect waves-light"><a href="' . $social_links['facebook_link'] . '" target="_blank"><i class="fa fa-facebook"></i></a></li>' : '';
-					echo !empty($social_links['twitter_link']) ? '<li class="waves-effect waves-light"><a href="' . $social_links['twitter_link'] . '" target="_blank"><i class="fa fa-twitter"></i></a></li>' : '';
-					echo !empty($social_links['linkedin_link']) ? '<li class="waves-effect waves-light"><a href="' . $social_links['linkedin_link'] . '" target="_blank"><i class="fa fa-linkedin"></i></a></li>' : '';
-					?>
-				</ul>
-			</div>
-		</div>
-	</footer><!-- #colophon -->
+<footer id="colophon" class="site-footer" role="contentinfo">
+    <div class="footer-container row grey darken-3">
+        <div class="container">
+            <?php dynamic_sidebar('at_footer_area'); ?>
+        </div>
+    </div>
+    <div class="footer-container-small">
+        <div class="container">
+            <div class="left copyright-container">
+                <span>Copyright 2016. All rights reserved.</span>
+            </div>
+            <div class="social-container grey darken-3 right">
+                <ul>
+                    <?php
+                    $social_links = get_option('atelier_social_media_options');
+                    echo !empty($social_links['facebook_link']) ? '<li class="waves-effect waves-light"><a href="' . $social_links['facebook_link'] . '" target="_blank"><i class="fa fa-facebook"></i></a></li>' : '';
+                    echo !empty($social_links['twitter_link']) ? '<li class="waves-effect waves-light"><a href="' . $social_links['twitter_link'] . '" target="_blank"><i class="fa fa-twitter"></i></a></li>' : '';
+                    echo !empty($social_links['linkedin_link']) ? '<li class="waves-effect waves-light"><a href="' . $social_links['linkedin_link'] . '" target="_blank"><i class="fa fa-linkedin"></i></a></li>' : '';
+                    ?>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
