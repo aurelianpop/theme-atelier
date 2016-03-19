c6e7fba27d96ef6dbf90a7500a0768ea7380c520
Pop Aurelian <office@elipsemedia.com>
2016-03-19 13:01:09 +0200

    Remove comments

--- partners-page.php
+++ partners-page.php
@@ -13,11 +13,6 @@ get_header(); ?>
 
                 get_template_part( 'template-parts/partners', 'list' );
 
-                // If comments are open or we have at least one comment, load up the comment template.
-                if ( comments_open() || get_comments_number() ) :
-                    comments_template();
-                endif;
-
             endwhile; // End of the loop.
             ?>
 
--- settings-page.php
+++ settings-page.php
@@ -19,11 +19,6 @@ get_header(); ?>
                     get_template_part( 'template-parts/settings', 'parents' );
                 }
 
-                // If comments are open or we have at least one comment, load up the comment template.
-                if ( comments_open() || get_comments_number() ) :
-                    comments_template();
-                endif;
-
             endwhile; // End of the loop.
             ?>
 
