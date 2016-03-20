d0fa172daac5b14028976370b1ff15b3da24a66c
Pop Aurelian <office@elipsemedia.com>
2016-03-20 20:46:32 +0200

    activity cards

--- archive-activities.php
+++ archive-activities.php
@@ -21,21 +21,20 @@ get_header(); ?>
 					the_archive_description( '<div class="taxonomy-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
-			<div class="row">
-				<?php
-				/* Start the Loop */
-				while ( have_posts() ) : the_post();
 
-					/*
-					 * Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'template-parts/content', 'archive' );
-
-				endwhile; ?>
-			</div>
 			<?php
+			/* Start the Loop */
+			while ( have_posts() ) : the_post();
+
+				/*
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content', get_post_format() );
+
+			endwhile;
+
 			the_posts_navigation();
 
 		else :
