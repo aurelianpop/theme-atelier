dd9e01fbab4bf5c5b4874a11672bd178d0624d5d
Pop Aurelian <office@elipsemedia.com>
2016-03-27 15:26:33 +0300

    Single page updates

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,7 +10,8 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php $feature_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
+	<?php  if( !is_single()) {
+	$feature_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
 	<div class="col s12 m4">
 		<div class="card hoverable">
 			<div class="card-image truncate">
@@ -18,12 +19,7 @@
 			</div>
 			<div class="card-content">
 				<?php
-				if ( is_single() ) {
-					the_title( '<h1 class="entry-title truncate">', '</h1>' );
-				} else {
-					the_title( '<h2 class="entry-title truncate"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-				}
-
+				the_title( '<h2 class="entry-title truncate"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 				if ( 'post' === get_post_type() || 'atelier_activities' === get_post_type() ) : ?>
 					<div class="entry-meta">
 						<?php theme_atelier_posted_on(); ?>
@@ -45,6 +41,39 @@
 			</div>
 		</div>
 	</div>
+	<?php } else { ?>
+		<header class="entry-header">
+			<?php
+			the_title( '<h1 class="entry-title">', '</h1>' );
+
+			if ( 'post' === get_post_type() ) : ?>
+				<div class="entry-meta">
+					<?php theme_atelier_posted_on(); ?>
+				</div><!-- .entry-meta -->
+				<?php
+			endif; ?>
+		</header><!-- .entry-header -->
+
+		<div class="entry-content">
+			<?php
+			the_content( sprintf(
+			/* translators: %s: Name of current post. */
+				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'theme-atelier' ), array( 'span' => array( 'class' => array() ) ) ),
+				the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			) );
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'theme-atelier' ),
+				'after'  => '</div>',
+			) );
+			?>
+		</div><!-- .entry-content -->
+
+		<footer class="entry-footer">
+			<?php theme_atelier_entry_footer(); ?>
+		</footer><!-- .entry-footer -->
+	<?php } ?>
+
 	<?php
 	wp_link_pages( array(
 		'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'theme-atelier' ),
